8e652090420df058d4161d07a4652532e1f4ed5d
Rajpateriya <pateriyaraj52@gmail.com>
2024-08-15 21:09:44 +0530

    error handling

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/login', async (req, res) => {
 
   try {
     const user = await User.findOne({ username });
-    // if (!user) return res.status(400).json({ error: 'User not found' });
+    if (!user) return res.status(400).json({ error: 'User not found' });
 
     // const isMatch = await bcrypt.compare(password, user.password);
     // if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
